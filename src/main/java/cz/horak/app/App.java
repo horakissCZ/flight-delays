6567eb8f9f518adda1e881be8f26998d3b970821
Jakub Horák <jakubhorak@outlook.cz>
2019-05-25 21:38:54 +0200

    Set the basic configuration for logging. Add ArchiveManager that uncompress a bzip2 format of the file.

--- src/main/java/cz/horak/app/App.java
+++ src/main/java/cz/horak/app/App.java
@@ -1,5 +1,16 @@
 package cz.horak.app;
 
-public class App {
+import org.apache.log4j.BasicConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+public class App {
+    
+    static final Logger logger = LoggerFactory.getLogger(App.class);
+    
+    public static void main(String[] args) {
+        
+        BasicConfigurator.configure();
+        
+    }
 }
